fe43e13a4db22fe2c83b651dcdfa37002701fe71
Shayane Tan <shayane_tan@yahoo.com>
2017-06-24 01:13:02 +0800

    added method to retrieve title of webpages

--- features/Links.py
+++ features/Links.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_keywords(link):
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup.findAll(name='meta', attrs={'name':'keywords'})[0]['content'].split(", "))
+class Links:
+    def get_keywords(link):
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return soup.findAll(name='meta', attrs={'name':'keywords'})[0]['content'].split(", ")
+
+    def get_title(link):
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return soup.title.string
+
+
+
+#link = "https://t.co/F5uvLaNUS6"
+#print("title:", Links.get_title(link))
