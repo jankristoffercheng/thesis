ee6e4b50a0411274f98955a45dac56781b10885d
Shayane Tan <shayane_tan@yahoo.com>
2017-06-25 21:32:15 +0800

    combined facebook pos

--- combinepos.py
+++ combinepos.py
@@ -5,7 +5,7 @@ from model.Post import Post
 postsDAO = PostsDAO()
 posFeature = POSFeature()
 posFeature.populateMappingDictionary()
-posts = postsDAO.getPosts(529677)
+posts = postsDAO.getPosts(1322926)
 
 #posts = []
 #posts.append(Post('00000417459', 'Kimi No Na Wa. Ang ganda please. Lalo na yung OST.', 'NNP-NNP-NNP-NNP-.-NNP-JJ-NN-.-NNP-TO-VB-NNP-.', 'UNK-NNP-CCP-NNPA-DTC-NNC-NNC-PMP-PMP-CCP-PRO-UNK-PMP'))
